2eee0ebae53c0e008f931987718356b1ac19b39e
Guilherme Mazzoni <guicm120@gmail.com>
2026-09-16 18:22:04 -0300

    frontend: Add Explainability widget with SHAP plot

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,6 @@
 import SlaPredictorBento from './components/SlaPredictorBento'
 import AiAnalystBento from './components/AiAnalystBento'
+import ExplainabilityBento from './components/ExplainabilityBento'
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
       <main className="bento-grid">
         <SlaPredictorBento />
         <AiAnalystBento />
+        <ExplainabilityBento />
       </main>
     </div>
   )
